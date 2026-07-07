2be6ee8ac21a5962b4c6c3487b78270b911a8865
Konstantinos Panayiotou <klpanagi@gmail.com>
2026-07-07 19:09:31 +0300

    Cleanup

--- demos/bdd/login/cucumber.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  default: {
-    paths: ['*.feature'],
-    require: ['tests/**/*.ts', 'tests/**/*.steps.ts'],
-    format: ['progress', 'html:reports/cucumber-report.html'],
-    formatOptions: { snippetInterface: 'async-await' },
-    publishQuiet: true,
-  },
-};
--- demos/bdd/login/tests/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export { LoginPage } from './LoginPage.page';
-export { HomePage } from './HomePage.page';
